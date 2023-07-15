2b4141f7b9b8aeda45defbbb933bc916fa9cc1e0
Laura Comte <comtelaurapro@gmail.com>
2023-07-16 01:12:37 +0200

    cross + trash
    trash disabled
    delete todo, cross the last one (to fix)

--- todo_react/src/components/main/main.jsx
+++ todo_react/src/components/main/main.jsx
@@ -1,26 +1,49 @@
 import "./main.scss";
+import trash from "../../assets/trash-can-solid.svg";
 import { useState } from "react";
 import Todo from "../todo/todo.jsx";
 
 const Main = (props) => {
   // const {} = props;
   const [inputValue, setinputValue] = useState("");
-  let arrayInput = [];
+  const [arrayInput, setArrayInput] = useState([]);
 
   const UpdateValue = (value) => {
     setinputValue(value);
-    console.log(inputValue);
   };
 
-  const handleClick = () => {
+  const handleClick = async () => {
     if (!inputValue || inputValue === "") {
       return;
     }
-    arrayInput.push(inputValue);
-    console.log(arrayInput);
-    //stocker inputValue dans un endroit (tableau ou localStorage) pour transmettre a Todo, et effacer l'input. Boucle forEach pour tableau, dans return avec {} qui entoure Todo et je lui passerais plus inputValue mais la valeur du tableau.
-    //tableau crée et transmis a Todo, forEach bien placé? Pour la liste ne s'incrémente pas? (la valeur écrase la précèdente) et pourquoi elle n'apparait pas dans la div visiuellement
+    if (arrayInput.length === 0) {
+      setArrayInput([
+        {
+          value: inputValue,
+          isComplete: false,
+          counter: new Date().valueOf(),
+        },
+      ]);
+    } else {
+      setArrayInput([
+        ...arrayInput,
+        { value: inputValue, isComplete: false, counter: new Date().valueOf() },
+      ]);
+    }
+
     setinputValue("");
+  };
+
+  let deleteTodo = () => {
+    setArrayInput(
+      arrayInput.filter((element) => {
+        if (element.isComplete === true) {
+          return false; //ne push pas l'element dans le tableau car false (spécialité filter)
+        } else {
+          return true; //push l'element dans le tableau car true (spécialité filter)
+        }
+      })
+    );
   };
 
   return (
@@ -38,7 +61,18 @@ const Main = (props) => {
           Ajouter à la liste !
         </button>
       </div>
-      <Todo arrayInput={arrayInput.forEach((element) => element)} />
+      {arrayInput.map((element, index) => (
+        <Todo key={index} arrayInput={element} />
+      ))}
+      <div className="trash-can" onClick={(e) => deleteTodo()}>
+        <img
+          src={trash}
+          className="svg"
+          alt="trash"
+          height="1em"
+          viewBox="0 0 448 512"
+        ></img>
+      </div>
     </div>
   );
 };
--- todo_react/src/components/todo/todo.jsx
+++ todo_react/src/components/todo/todo.jsx
@@ -4,9 +4,26 @@ import { useState } from "react";
 const Todo = (props) => {
   const { arrayInput } = props;
 
+  let toCompleted = () => {
+    let span = document.getElementById(arrayInput.counter);
+    if (span.classList.contains("cross")) {
+      span.classList.remove("cross");
+      arrayInput.isComplete = false;
+    } else {
+      span.classList.add("cross");
+      arrayInput.isComplete = true;
+    }
+  };
+
   return (
-    <div className="inputValueDiv">
-      <span>{arrayInput}</span>
+    <div className="inputValueDiv" onClick={(e) => toCompleted()}>
+      {arrayInput.isComplete ? (
+        <span id={arrayInput.counter} className="cross">
+          {arrayInput.value}
+        </span>
+      ) : (
+        <span id={arrayInput.counter}>{arrayInput.value}</span>
+      )}
     </div>
   );
 };
